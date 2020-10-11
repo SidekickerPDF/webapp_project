5c66be2eb21f71a5b7bc26e5c031c03f99e3f276
Rashbir Singh <rashbirkohli@gmail.com>
2020-10-12 02:42:05 +1100

    Part 8

---
+++ webapp/test.py
@@ -0,0 +1,137 @@
+import spacy
+
+nlp = spacy.load("en_blackstone_proto")
+doc = nlp("""  H. B. Higgins
+ Henry Bournes Higgins KC (30 June 1851 – 13 January 1929) was an Australian lawyer, politician, and judge. He served on the High Court of Australia from 1906 until his death in 1929, after briefly serving as Attorney-General of Australia in 1904.
+Higgins was born in what is now Northern Ireland. He and his family immigrated to Australia when he was 18, and he found work as a schoolteacher while studying law part-time at the University of Melbourne. He was admitted to the Victorian Bar in 1876, and built up a substantial practice specialising in equity law. Higgins came to public attention as a prominent supporter of Irish Home Rule. He was elected to the Victorian Legislative Assembly in 1894, and represented Victoria at the Australasian Federal Convention, where he helped draft the new federal constitution. He nonetheless opposed the final draft, making him one of only two delegates to the convention to campaign against federation.
+In 1901, Higgins was elected to the new federal parliament as a member of the Protectionist Party. He was sympathetic to the labour movement, and in 1904 briefly served as Attorney-General in the Labor Party minority government led by Chris Watson. In 1906, Prime Minister Alfred Deakin decided to expand the High Court bench from three to five members, nominating Higgins and Isaac Isaacs to the court. Higgins was usually in the minority in his early years on the court, but in later years the composition of the court changed and he was more often in the majority. He also served as president of the Commonwealth Court of Conciliation and Arbitration from 1907 to 1921. In that capacity, he wrote the decision in the influential Harvester case, holding that a legislative provision for a "fair and reasonable" wage effectively required a living wage.
+In office
+13 October 1906 – 13 January 1929
+ The Honourable
+Henry Bournes Higgins
+KC
+                      Justice of the High Court of Australia
+        Nominated by
+Appointed by Preceded by Succeeded by
+Alfred Deakin
+Lord Northcote
+none (new seat)
+Sir Owen Dixon
+In office
+                      Attorney-General of Australia
+   27 April 1904 – 17 August 1904
+ Prime Minister
+Preceded by Succeeded by
+Chris Watson
+James Drake Josiah Symon
+In office
+                 Member of the Australian Parliament for Northern Melbourne
+     Contents
+Early life and education Colonial politics Federal politics
+30 March 1901 – 12 October 1906
+Preceded by Seat Created
+Succeeded by Seat Abolished
+        Personal details
+    
+      High Court
+Harvester case World War I Final years
+Personal life Legacy References Further reading
+           Born 30 June 1851
+Newtownards, County Down,
+Ireland
+        Relations Nettie Palmer (niece) George Ernest Morrison
+(brother-in-law)
+  Died
+Political party Spouse(s)
+13 January 1929 (aged 77) Dromana, Victoria, Australia
+Protectionist
+Mary Alice Morrison (m. 1885)
+          Early life and education
+ He was born in Newtownards, County Down, Ireland, the son of The Rev. John Higgins,[1] a Methodist minister, and Anne Bournes, daughter of Henry Bournes of Crossmolina. Ina Higgins, an early feminist, was his sister and Nettie Palmer, poet, essayist and literary critic, was a niece. The Rev. Higgins and his family emigrated to Australia in 1870.
+H. B. Higgins was educated at Wesley College in central Dublin, Ireland, and at the University of Melbourne, where he graduated in law. He practised at the Melbourne bar from 1876, eventually becoming one of the city's leading barristers (a KC in 1903) and a wealthy man. He was active in liberal, radical, and Irish nationalist politics, as well as in many civic organisations. He was also a noted classical scholar.
+Colonial politics
+               In 1894, Higgins was elected to the Victorian Legislative Assembly as MLA for Geelong. He was a supporter of George Turner's liberal government, but frequently criticised it from a left-wing point of view. He supported advanced liberal positions, such as greater protection for workers, government investment in industry, and votes for women. In 1897, he was elected as one of Victoria's delegates to the convention which drew up the Australian Constitution. At the convention, he successfully argued that the constitution should contain a guarantee of religious freedom, and also a provision giving the federal government the power to make laws relating to the conciliation and arbitration of industrial disputes.
+Despite these successes, he opposed the draft constitution produced by the convention as too conservative, and campaigned unsuccessfully to have it defeated at the 1899 Australian constitutional referendum. This alienated him most of his liberal colleagues, and also from the influential Melbourne newspaper, The Age. Higgins also opposed Australian involvement in the Second Boer War, a very unpopular stand at the time, and as a result, he lost his seat at the 1900 Victorian election.
+Federal politics
+          In 1901, when federation under the new constitution came into effect, Higgins was elected to the first House of Representatives for the working-class electorate of Northern Melbourne. He stood as a Protectionist, but the Labor Party did not oppose him, regarding him as a supporter of the labour movement. The Labor Party's confidence in him was shown in 1904, when Chris Watson formed
+     
+ the first federal Labor government. Since the party did not have a suitably qualified lawyer, Watson offered the post of Attorney-General to Higgins. He is the only person to have held office in a federal Labor government without being a member of the Labor Party.
+Robert Garran, the Secretary of the Attorney-General's Department, found Higgins difficult to
+work with, stating that "for the first week or two I could not induce him to sign even the most
+routine and trivial paper until after a full explanation [...] the slowing-up of the machine was often
+[3]
+According to Ross McMullin, Higgins
+  [2]
+which left Watson and Billy Hughes having to explain his ministerial actions. The Melbourne
+embarrassing".
+Higgins also received criticism for his failure to regularly attend parliament,
+  Argus remarked that he "attends sometimes for prayers and retires spiritually refreshed".
+was also attacked by The Bulletin, which observed that he "has had an unparalleled opportunity to demonstrate his worth as legal ally of the Government, and all the demonstration that he has
+[4]
+He
+  effected in the House could be contained in a paper bag".
+ "approved of the Labor Party and its objectives [...] but he
+remained outside the party and did not identify himself with
+ [5]
+appreciated by his colleagues, and Watson was happy to
+it".
+defend him against the attacks from the media.
+However, his contributions to cabinet discussions were [6]
+High Court
+Higgins was an awkward colleague for the Protectionist leadership, and in 1906 Deakin appointed him as a Justice of the High Court of Australia as a means of getting him out of politics, although he was undoubtedly qualified for the post. In 1907, he was also appointed President of the newly created Commonwealth Court of Conciliation and Arbitration, created to arbitrate disputes between trades unions and employers, something Higgins had long advocated. In this role, he continued to support the labour movement, although he was strongly opposed to militant unions who abused the strike weapon and ignored his rulings.
+Henry Bournes Higgins at the 1898 Australasian Federal Convention.
+     Higgins was one of only eight justices of the High Court to have served in the Parliament of Australia prior to his appointment to the Court; the others were Edmund Barton, Richard O'Connor, Isaac Isaacs, Edward McTiernan, John Latham, Garfield Barwick, and Lionel Murphy. He was also one of two justices to have served in the Parliament of Victoria, along with Isaac Isaacs.
+Harvester case
+In 1907, Higgins delivered a judgement which became famous in Australian history, known as the "Harvester Judgement". The case involved one of Australia's largest employers, Hugh McKay, a manufacturer of agricultural machinery. Higgins ruled that McKay was obliged to pay his employees a wage that guaranteed them a standard of living that was reasonable for "a human
+being in a civilised community", regardless of his capacity to pay. This gave rise to the legal
+[7]
+              requirement for a basic wage, which dominated Australian economic life for the next 80 years.
+ 
+ World War I
+During World War I, Higgins increasingly came into conflict with the Nationalist Prime Minister Billy Hughes, whom he saw as using the wartime emergency to erode civil liberties. Although Higgins initially supported the war, he opposed the extension of government power that came with it, and also opposed Hughes' attempt to introduce conscription for the war. In 1916, his only son Mervyn was killed in action in Egypt, a tragedy which made Higgins turn increasingly against the war.
+Final years
+      The postwar years saw a series of bitter industrial
+confrontations, some of them fomented by militant unions
+influenced by the Industrial Workers of the World or the
+Communist Party of Australia. Higgins defended the principles
+of arbitration against both the Hughes Government and
+militant unions, although he found this increasingly difficult. Postwar governments appointed conservative justices to the High Court, leaving Higgins increasingly more isolated. In 1920, he resigned from the Arbitration Court in frustration, but remained on the High Court bench until his death in 1929. In 1922, he published A New Province for Law and Order, a defence of his views and record on arbitration.
+Personal life
+Henry Bournes Higgins
+    On 19 December 1885, Higgins married Mary Alice Morrison, the daughter of George Morrison, headmaster of Geelong College, and the sister of the journalist George Ernest Morrison. Their only child, Mervyn Bournes Higgins, was born in 1887, and was killed in action in Egypt in 1916.
+After his son Mervyn's death, Higgins effectively adopted his nephew Esmonde Higgins and his niece Nettie Palmer, paying for their education at universities in Europe. He was pained by Esmonde's conversion to Communism in 1920 and his rejection of the liberal values associated with the Higgins name.
+Aside from politics, he was president of the Carlton Football Club in 1904.
+Legacy
+Higgins and his wife Mary (née Morrison)
+        Higgins was remembered for many years as a great friend of the labour movement, of the Irish- Australian community and of liberal and progressive causes generally. He was well-served by his first biographer, his niece Nettie Palmer, whose Henry Bournes Higgins: A Memoir (1931) created
+
+ an enduring Higgins mythology. John Rickard's 1984 H. B. Higgins: The Rebel as Judge partly demolished this myth, but was a generally sympathetic biography. The H.B. Higgins Chambers in Sydney, founded by radical industrial lawyers, is named for him.
+Further, Higgins is commemorated by the federal electorate of Higgins in Melbourne, and by the Canberra suburb of Higgins, Australian Capital Territory.
+References
+     1. Irish Independent, 8 February 1906. p. 7.
+2. Ross McMullin (2004). So Monstrous a Travesty: Chris Watson and the World's First National
+Labour Government. Scribe Publications. p. 98.
+3. McMullin (2004), p. 99.
+4. McMullin (2004), p. 100.
+5. McMullin (2004), p. 101.
+6. McMullin (2004), p. 102.
+7. T.J. Kearney, The life and times of Henry Bournes Higgins, politician and judge (1851-1929), Journal of the Australian Catholic Historical Society, 11 (1989), 33-43.
+Further reading
+  Rickard, John (1983). "Higgins, Henry Bournes (1851–1929)" (http://adb.anu.edu.au/biography /higgins-henry-bournes-6662). Australian Dictionary of Biography. Retrieved 25 January 2019.
+Rickard, John (1984). H. B. Higgins, the Rebel as Judge. Sydney: Allen and Unwin. ISBN 0- 86861-681-8.
+         Preceded by
+James Drake
+New division
+Political offices Attorney-General of Australia
+1904
+Parliament of Australia Member for Northern Melbourne
+1901–1906
+Succeeded by
+Josiah Symon
+Division abolished
+                 Retrieved from "https://en.wikipedia.org/w/index.php?title=H._B._Higgins&oldid=953998219"
+  This page was last edited on 30 April 2020, at 04:09 (UTC).
+Text is available under the Creative Commons Attribution-ShareAlike License; additional terms may apply. By using this site, you agree to the Terms of Use and Privacy Policy. Wikipedia® is a registered trademark of the Wikimedia Foundation, Inc., a non-profit organization.
+""")
+for w in doc.ents:
+    print(w.text, w.label_, "\n")

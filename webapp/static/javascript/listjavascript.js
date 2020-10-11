1ff2feccd7049058952c222703b48ae525a3561d
Rashbir Singh <rashbirkohli@gmail.com>
2020-10-12 02:32:47 +1100

    Part 7

---
+++ webapp/static/javascript/listjavascript.js
@@ -0,0 +1,26 @@
+var toggler = document.getElementsByClassName("caret");
+var i;
+
+for (i = 0; i < toggler.length; i++) {
+  toggler[i].addEventListener("click", function() {
+    this.parentElement.querySelector(".nested").classList.toggle("active");
+    this.classList.toggle("caret-down");
+  });
+}
+
+ $(document).ready(function() {
+
+   $('span').click(function() {
+   $(this).nextUntil('span').toggle();
+   $('.table').css('display', 'table');
+   });
+
+    $('.clickme').click(function() {
+     $('.firstlist').toggle();
+   });
+ });
+
+
+
+
+
